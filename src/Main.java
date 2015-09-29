43443c1da48e9ed976050abc7ceba0161f8ee5f2
Eduardo Fernandes <e.luis@icloud.com>
2015-09-29 12:40:24 +0100

    Add empty JavaFX window

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    private final static String appTitle = "TITLE";
+
+    /**
+     * Entry point
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Main.fxml"));
+        Parent root = fxmlLoader.load();
+
+        Platform.setImplicitExit(false);
+
+        // primaryStage.setOnCloseRequest(event -> Platform.exit());
+
+        primaryStage.setTitle(appTitle);
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+}
