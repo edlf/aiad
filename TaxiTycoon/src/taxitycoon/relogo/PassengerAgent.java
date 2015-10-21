2730350c81fc0fb11c95712d814680a580c4a251
Eduardo Fernandes <ei12130@fe.up.pt>
2015-10-21 11:22:59 +0100

    Update project

--- TaxiTycoon/src/taxitycoon/relogo/PassengerAgent.java
+++
@@ -1,7 +0,0 @@
-package taxitycoon.relogo;
-
-import sajas.core.Agent;
-
-public class PassengerAgent extends Agent {
-
-}
--- TaxiTycoon/src/taxitycoon/relogo/TaxiAgent.java
+++
@@ -1,7 +0,0 @@
-package taxitycoon.relogo;
-
-import sajas.core.Agent;
-
-public class TaxiAgent extends Agent {
-
-}
